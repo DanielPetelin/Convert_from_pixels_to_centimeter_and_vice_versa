3dcb4c14373497b4615bb9c069d0bbb6f687eaa8
DanielPetelin <daniilpetelin77@gmail.com>
2020-04-04 16:55:13 +0300

    add new fun

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
-document.querySelector('.push').onclick = () => {
+function letCm() {
     let px = 38;
     // let cm = 1;
-    let yourPx = document.querySelector('#px').value;
+    let yourPx = +document.querySelector('#px').value;
     let result = yourPx / px;
 
     document.querySelector('#out').innerHTML = yourPx + "px = " + result + 'см'
+};
+
+function letPx() {
+    let px = 38;
+    // let cm = 1;
+    let yourCm = +document.querySelector('#cm').value;
+    let result = yourCm * px;
+
+    document.querySelector('#out2').innerHTML = yourCm + "см = " + result + 'px'
 };
